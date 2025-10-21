7655dde2122c6b4a623859f3763abb33a637eef0
Pinkos1 <Pinkos2003@gmail.com>
2025-10-21 10:42:35 -0400

    implementation of quicksort

--- sorting_algorithm_project.py
+++ sorting_algorithm_project.py
@@ -41,6 +41,39 @@ def insertion_sort(arr):
 
 
 
+""" Randomize Quicksort Implementation"""
+
+
+def partition(arr, left, right):
+    pivot = arr[right]
+    i = left - 1
+    
+    for j in range(left, right):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+        arr[i + 1], arr[right] = arr[right], arr[i + 1]
+
+    return i + 1
+
+
+
+def randomize_partition(arr, left, right):
+    pivot_index = random.randint(left, right)
+    arr[pivot_index], arr[right] = arr[right], arr[pivot_index]  
+
+    return partition(arr, left, right)
+
+
+
+def randomized_quicksort(arr, left, right):
+    if left < right:
+        pivot_index = randomize_partition(arr, left, right)
+        
+        randomized_quicksort(arr, left, pivot_index - 1)
+        randomized_quicksort(arr, pivot_index + 1, right)
+
         
 
 
