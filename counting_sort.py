909c8ce116ea9569f7329f31a089caf6ff69465c
Pinkos1 <Pinkos2003@gmail.com>
2025-10-28 17:34:44 -0400

    implemented counting sort algorithm

--- counting_sort.py
+++ counting_sort.py
@@ -14,3 +14,32 @@ import random
 import time
 
 
+
+def counting_sort(arr, k):
+    n = len(arr)
+    x = [0] * n        
+    y = [0] * (k + 1)  
+
+    # Loop 1
+    for i in range(k + 1):
+        y[i] = 0
+
+    # Loop 2
+    for j in range(n):
+        y[arr[j]] = y[arr[j]] + 1
+
+    # Loop 3
+    for i in range(1, k + 1):
+        y[i] = y[i] + y[i - 1]
+
+    # Loop 4 stable
+    for j in range(n - 1, -1, -1):
+        x[y[arr[j]] - 1] = arr[j]
+        y[arr[j]] = y[arr[j]] - 1
+
+    return x
+
+
+
+
+
